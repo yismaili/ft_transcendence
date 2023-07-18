53e2e7990a2833d36a12e0a8f4a9d07f01c91a69
younes ismaili <younesismaili20@gmail.com>
2023-07-18 14:12:23 +0100

    trying to make authentication

--- back_end/transcendence/src/app.module.ts
+++ back_end/transcendence/src/app.module.ts
@@ -2,20 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
 
-@Module({
-  imports: [ 
+// The @Module() decorator marks the AppModule class as a module in NestJS
+// The imports property specifies the modules that this module depends on
+// The AppModule serves as the entry point and the root module of your application
+
+
+@Module({ // decorator It is used to define a module in a NestJS application
+  imports: [ //import the TypeOrmModule and configure it for connecting to a PostgreSQL database.
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'postgres_db',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass1337',
-    database: 'transcendence',
-    autoLoadEntities: true,
-    synchronize: true,//in tisting in prodection fales
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+      type: 'postgres',
+      host: 'postgres_db',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass1337',
+      database: 'transcendence',
+      entities: [User],
+      autoLoadEntities: true, // automatically load entity files
+      synchronize: true, // automatically synchronize the database schema with the entities. // Set to false in production
+    }), 
+    UserModule,
+  ],
+  controllers: [AppController], // Controllers  handle incoming requests and define the routes and endpoints for the application
+  providers: [AppService], // Providers are responsible for providing business logic and functionality to the application.
 })
-export class AppModule {}
+export class AppModule { // class that represents the main module of application
+
+}
---
+++ back_end/transcendence/src/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
