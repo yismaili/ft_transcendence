ae2f10b1207135583dd5d5ee65bee2b4cd9fb201
younes ismaili <younesismaili20@gmail.com>
2023-07-18 11:46:17 +0100

    make database

--- back_end/transcendence/src/app.module.ts
+++ back_end/transcendence/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ 
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'postgres_db',
+    port: 5432,
+    username: 'postgres',
+    password: 'pass1337',
+    database: 'transcendence',
+    autoLoadEntities: true,
+    synchronize: true,//in tisting in prodection fales
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
