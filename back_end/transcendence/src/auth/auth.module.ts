ddd25fe3527e6a8d72e085bf18339b820410b5d6
younes ismaili <younesismaili20@gmail.com>
2023-07-21 09:40:49 +0100

    hashed Password

--- back_end/transcendence/src/auth/auth.module.ts
+++ back_end/transcendence/src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PasswordHashingService } from 'src/password-hashing/password-hashing.service';
 //responsible for defining the components related to authentication and user management
 @Module({
   // forFeature() method is used to specify which entities 
   imports: [TypeOrmModule.forFeature([User])], // makes the User entity available for use within the AuthModule
   controllers: [AuthController],// The controllers property
-  providers: [AuthService]
+  providers: [AuthService, PasswordHashingService]
 }) // decorator is define a module 
 export class AuthModule {}
