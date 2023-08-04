06e712b92d472de2304bc3446a52eefc9dfb0246
yismaili <yismaili@student.1337.ma>
2023-08-04 14:22:35 +0100

    make APIs of profile to update data

--- back_end/transcendence/src/user/user.controller.ts
+++ back_end/transcendence/src/user/user.controller.ts
@@ -43,10 +43,10 @@ export class UserController {
         }
     }
 
-    @Put('update/profile/:id')
-    async updateProfile(@Param('id', ParseIntPipe) id: number, @Body() updateProfilerDto: ProfileDto,){
-        this.userService.updateProfile(id, updateProfilerDto);
-    }
+    // @Put('update/profile/:id')
+    // async updateProfile(@Param('id', ParseIntPipe) id: number, @Body() updateProfilerDto: ProfileDto,){
+    //     this.userService.updateProfile(id, updateProfilerDto);
+    // }
 
     // @Put('update/Achievement/:id')
     // async updateAchievement(@Param('id', ParseIntPipe) id: number, @Body() updateAchievementDto: AchievementDto,){
