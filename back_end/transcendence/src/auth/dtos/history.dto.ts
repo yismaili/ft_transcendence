07ff86334e8934c4390014dd809eae4de73601b6
yismaili <yismaili@student.1337.ma>
2023-08-05 19:11:10 +0100

    make APIs of history to get all users Competitors

--- back_end/transcendence/src/auth/dtos/history.dto.ts
+++ back_end/transcendence/src/auth/dtos/history.dto.ts
@@ -1,7 +1,6 @@
 import { User } from "src/typeorm/entities/User.entity";
 
-export class HistoryDto{
-    user:User
-    date: Date;
-    userCompetitor: User;
+export interface HistoryDto {
+  id: number;
+  userCompetitor: User;
 }
