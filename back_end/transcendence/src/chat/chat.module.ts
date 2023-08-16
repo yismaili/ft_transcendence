d1bfddd83aa98fc5b00f1d524f5c98aa83ba0a2c
yismaili <yismaili@student.1337.ma>
2023-08-16 15:09:26 +0100

    make websocket to build chat app

--- back_end/transcendence/src/chat/chat.module.ts
+++ back_end/transcendence/src/chat/chat.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { UserService } from 'src/user/user.service';
+import { Chat } from 'src/typeorm/entities/chat.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/typeorm/entities/User.entity';
+import { Profile } from 'src/typeorm/entities/Profile.entity';
+import { Relation } from 'src/typeorm/entities/Relation.entity';
+import { HistoryEntity } from 'src/typeorm/entities/History.entity';
+import { Achievement } from 'src/typeorm/entities/Achievement.entity';
+import { UserModule } from 'src/user/user.module';
+
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forFeature([User, Profile, Relation, HistoryEntity, Achievement, Chat])
+  ],
+  // controllers: [ChatGateway],
+  providers: [ChatService, UserService, AuthModule, ChatGateway],
 })
 export class ChatModule {}
