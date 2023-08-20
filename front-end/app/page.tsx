ee6f001cb5532a4885b7bcec6cad688ce1431a0e
alouane04 <ariahi@student.1337.ma>
2023-08-20 07:39:57 +0100

    start of the implementation

---
+++ front-end/app/page.tsx
@@ -0,0 +1,11 @@
+// import Image from 'next/image'
+// import styles from './page.module.css'
+import Nav from "@/components/nav/Nav"
+
+export default function Home() {
+  return (
+    <header>
+      <Nav/>
+    </header>
+  )
+}
---
+++ front-end/components/nav/Nav.tsx
@@ -0,0 +1,11 @@
+import Logo from './Logo'
+import Links from './Links';
+
+export default function Nav() {
+    return (
+        <div>
+            <Logo/>
+            <Links/>
+        </div>
+    );
+};
