a934ea43dc59bc96a04f76757b37b4c3c67094e7
alouane04 <ariahi@student.1337.ma>
2023-08-20 18:51:42 +0100

    done for today still need the button and stuff

--- front-end/components/nav/Nav.tsx
+++ front-end/components/nav/Nav.tsx
@@ -1,11 +1,30 @@
-import Logo from './Logo'
-import Links from './Links';
+import Image from "next/image";
+import Link from "next/link";
+import Style from './Nav.module.css'
 
 export default function Nav() {
     return (
-        <div>
-            <Logo/>
-            <Links/>
-        </div>
+        <header>
+            <ul className={Style.container}>
+                <li className={Style.imageRes}>
+                    <Image 
+                    quality={100}
+                    className={Style.imageRes} 
+                    width={75}
+                    height={75}
+                    src="/logo.png" alt="logo for pong game" 
+                    priority={true}/>
+                </li>
+                <li>
+                    <h1 className={Style.title}>PONG</h1>
+                </li>
+                <li>
+                    <Link href="#">home</Link>
+                    <Link href="#">about</Link>
+                    <Link href="#">faq</Link>
+                    <Link href="#">Sign up</Link>
+                </li>
+            </ul>
+        </header>
     );
 };
