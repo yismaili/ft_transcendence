6644adbc8b6d45f701ade3e5ad76a0f06755b2a1
alouane04 <ariahi@student.1337.ma>
2023-08-21 21:36:14 +0100

    the hero section prot done

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,9 +1,13 @@
 // import styles from './page.module.css'
 import Nav from "@/components/nav/Nav"
+import Main from "@/components/main/Main"
 import '/styles/global.css'
 
 export default function Home() {
   return (
-      <Nav/>
+      <div>
+        <Nav/>
+        <Main/>
+      </div>
   )
 }
---
+++ front-end/components/Sections/Section1.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Style from "./Section1.module.css"
+import { ST } from "next/dist/shared/lib/utils";
+
+interface me {
+    className: string;
+}
+export default function Section1(props: me) {
+    return (
+        <section className={props.className}>
+            <div className={Style.Main}>
+                <h1>FUTURE OF <span>PONG</span></h1>
+                <p>START NOW</p>
+                <button>PLAY NOW</button>
+            </div>
+            <div className={Style.MainImg}>
+                <div className={Style.BallImg}>
+                    <Image
+                    src="/img/ball.png"
+                    alt="a ping ball image"
+                    priority={true}
+                    quality={100}
+                    fill/>
+                </div>
+                <div className={Style.MoonImg}>
+                    <Image
+                    src="/img/moon.png"
+                    alt="moon image"
+                    priority={true}
+                    quality={100}
+                    fill/>
+                </div>
+            </div>
+        </section>
+    );
+}
