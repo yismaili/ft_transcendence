1471548eae1118776070aa08073e0a337db6c903
alouane04 <ariahi@student.1337.ma>
2023-08-27 15:02:34 +0100

    cleanUP

--- front-end/components/Sections/Section3/AboutUs/AboutUs.tsx
+++ front-end/components/Sections/Section3/AboutUs/AboutUs.tsx
@@ -2,32 +2,32 @@ import Image from "next/image";
 import Style from "./AboutUs.module.css";
 
 export default function AboutUs() {
-    return (
-        <div className={Style.ParentDiv}>
-        <div className={Style.imageDiv}>
-          <Image
-            src="/img/section3/markman.png"
-            alt="a game image"
-            quality={100}
-            sizes="(max-width: 744px) 50vw, (max-width: 1200px) 25vw, 100vw"
-            width={100}
-            height={100}
-            className={Style.image}
-          />
-        </div>
-        <div className={Style.imageDiv2}>
-          <Image
-            src="/img/section3/arrow.png"
-            alt="a game image"
-            quality={100}
-            sizes="(max-width: 744px) 50vw, (max-width: 1200px) 25vw, 100vw"
-            width={100}
-            height={100}
-            className={Style.image2}
-          />
-        </div>
-        <h2>El Amine El Mountassir</h2>
-        <p>Front End Dev</p>
+  return (
+    <div className={Style.ParentDiv}>
+      <div className={Style.imageDiv}>
+        <Image
+          src="/img/section3/markman.png"
+          alt="a game image"
+          quality={100}
+          sizes="(max-width: 744px) 50vw, (max-width: 1200px) 25vw, 100vw"
+          width={100}
+          height={100}
+          className={Style.image}
+        />
       </div>
-    )
+      <div className={Style.imageDiv2}>
+        <Image
+          src="/img/section3/arrow.png"
+          alt="a game image"
+          quality={100}
+          sizes="(max-width: 744px) 50vw, (max-width: 1200px) 25vw, 100vw"
+          width={100}
+          height={100}
+          className={Style.image2}
+        />
+      </div>
+      <h2>El Amine El Mountassir</h2>
+      <p>Front End Dev</p>
+    </div>
+  );
 }
--- front-end/components/Sections/Section3/Section3.tsx
+++ front-end/components/Sections/Section3/Section3.tsx
@@ -1,4 +1,3 @@
-
 import AboutUs from "./AboutUs/AboutUs";
 import Style from "./Section3.module.css";
 
@@ -15,22 +14,3 @@ export default function Section3() {
     </section>
   );
 }
-
-// <div className={Style.container}>
-//                 <div className={Style.friend}>
-//                     <div className={Style.side__icons}>
-//                         <Image
-//                         src="/img/game.png"
-//                         alt="a game image"
-//                         quality={100}
-//                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-//                         fill
-//                         className={Style.side__icons__btn__icon}
-//                         />
-//                     </div>
-//                     <div>
-//                         <div className={Style.scrollElm}></div>
-//                         <div className={Style.scrollElm}></div>
-//                     </div>
-//                 </div>
-//             </div>
