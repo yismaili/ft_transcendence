bae9793b7f844ae8b8c01d1f089e671f801418f6
alouane04 <ariahi@student.1337.ma>
2023-08-27 23:37:03 +0100

    still need to make the hover effect

--- front-end/components/Sections/Section3/Section3.tsx
+++ front-end/components/Sections/Section3/Section3.tsx
@@ -1,17 +1,29 @@
-'use client'
+"use client";
 import AboutUs from "./AboutUs/AboutUs";
 import Style from "./Section3.module.css";
 
 export default function Section3() {
   return (
     <section className={Style.container}>
-		<AboutUs/>
-		<AboutUs/>
-		<AboutUs/>
-		<div>
-			<div className={Style.scrollElm}></div>
-			<div className={Style.scrollElm}></div>
-		</div>
+      <AboutUs
+        src="/img/section3/amine_black.jpeg"
+        name="El Amine El Mountassir"
+        title="Front End Dev"
+      />
+      <AboutUs
+        src="/img/section3/test.jpeg"
+        name="Ali Achraf Riahi"
+        title="Front End Dev"
+      />
+      <AboutUs
+        src="/img/section3/dexter.png"
+        name="Younes Ismaili"
+        title="Back End Dev"
+      />
+      <div>
+        <div className={Style.scrollElm}></div>
+        <div className={Style.scrollElm}></div>
+      </div>
     </section>
   );
 }
