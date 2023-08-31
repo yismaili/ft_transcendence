6063d60d874e6ccde9d56c10ad4ddd7c351c1902
alouane04 <alouane04@gmail.com>
2023-08-31 17:54:00 +0100

    finish the first fase for faq still some weird shit

--- front-end/components/main/Main.tsx
+++ front-end/components/main/Main.tsx
@@ -1,6 +1,7 @@
 import Section1 from "../Sections/Section1/Section1";
 import Section2 from "../Sections/Section2/Section2";
 import Section3 from "../Sections/Section3/Section3";
+import Section4 from "../Sections/Section4/Section4";
 import Style from "./Main.module.css";
 
 export default function Main() {
@@ -9,6 +10,7 @@ export default function Main() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </main>
   );
 }
