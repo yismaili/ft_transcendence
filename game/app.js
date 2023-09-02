56e2e1221c537b1462e62a2ccc076280bb3223d5
yismaili <yismaili@student.1337.ma>
2023-09-02 20:47:51 +0100

    update the game

--- game/app.js
+++ game/app.js
@@ -1,4 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jquery_1 = __importDefault(require("jquery"));
 class Canvas {
     constructor() {
         this.canvas = document.getElementById('canvas');
@@ -35,28 +40,26 @@ class Ball {
         context.closePath();
     }
     getSpeedX() {
-        return (this.speedX);
+        return this.speedX;
     }
     getSpeedY() {
-        return (this.speedY);
+        return this.speedY;
     }
 }
 class Paddle {
-    constructor(x, y, width, height, paddleSpeed, rightPaddle, leftPaddle) {
+    constructor(x, y, width, height, paddleSpeed) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.paddleSpeed = paddleSpeed;
-        this.rightPaddle = rightPaddle;
-        this.leftPaddle = leftPaddle;
     }
     draw(context) {
         context.fillStyle = '#ffffff';
         context.fillRect(this.x, this.y, this.width, this.height);
     }
     getPaddleSpeed() {
-        return (this.paddleSpeed);
+        return this.paddleSpeed;
     }
 }
 class MiddleLine {
@@ -89,20 +92,48 @@ class StartBtn {
     constructor() {
         this.startBtn = document.getElementById('start-btn');
         this.isRunning = false;
+        if (this.startBtn) {
+            this.startBtn.addEventListener('click', this.start.bind(this));
+        }
     }
     start() {
         if (!this.isRunning) {
-            // gameLoop();
+            // Implement game start logic here
             this.isRunning = true;
         }
     }
 }
-class KeyboardInput {
+class PongGame {
     constructor() {
+        this.leftPlayerScore = 0;
+        this.rightPlayerScore = 0;
         this.upPressed = false;
         this.downPressed = false;
         this.wPressed = false;
         this.sPressed = false;
+        this.canvas = new Canvas();
+        this.ball = new Ball(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2, 10, 10, 10);
+        this.leftPaddle_ = new Paddle(0, this.canvas.getHeight() / 2 - 50, 10, 100, 10);
+        this.rightPaddle_ = new Paddle(this.canvas.getWidth() - 10, this.canvas.getHeight() / 2 - 50, 10, 100, 10);
+        this.middleLine = new MiddleLine(this.canvas.getWidth() / 2, this.canvas.getHeight());
+        this.score = new Score(this.leftPlayerScore, this.rightPlayerScore);
+        this.ballX = this.canvas.getWidth() / 2;
+        this.ballY = this.canvas.getHeight() / 2;
+        this.ballSpeedX = 5;
+        this.ballSpeedY = 5;
+        this.ballRadius = 10;
+        this.player = '';
+        this.paddleHeight = 100;
+        this.paddleWidth = 10;
+        this.paddleSpeed = 10;
+        this.leftPaddle = this.canvas.getHeight() / 2 - this.paddleHeight / 2;
+        this.rightPaddle = this.canvas.getHeight() / 2 - this.paddleHeight / 2;
+        this.startBtn = document.getElementById('start-btn');
+        this.isRunning = false;
+        if (this.startBtn) {
+            this.startBtn.addEventListener('click', this.start.bind(this));
+        }
+        // Add keyboard event listeners
         document.addEventListener("keydown", this.keyDownHandler.bind(this));
         document.addEventListener("keyup", this.keyUpHandler.bind(this));
     }
@@ -134,54 +165,86 @@ class KeyboardInput {
             this.sPressed = false;
         }
     }
-    isUpPressed() {
-        return this.upPressed;
-    }
-    isDownPressed() {
-        return this.downPressed;
-    }
-    isWPressed() {
-        return this.wPressed;
-    }
-    isSPressed() {
-        return this.sPressed;
-    }
-}
-class update {
-}
-class PongGame {
-    constructor() {
-        this.leftPlayerScore = 0;
-        this.rightPlayerScore = 0;
-        this.canvas = new Canvas();
-        this.ball = new Ball(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2, 10, 10, 10);
-        this.leftPaddle = new Paddle(0, this.canvas.getHeight() / 2 - 50, 10, 100, 10, (this.canvas.getHeight / 2 - 50), (this.canvas.getHeight / 2 - 50));
-        this.rightPaddle = new Paddle(this.canvas.getWidth() - 10, this.canvas.getHeight() / 2 - 50, 10, 100, 10, (this.canvas.getHeight / 2 - 50), (this.canvas.getHeight / 2 - 50));
-        this.middleLine = new MiddleLine(this.canvas.getWidth() / 2, this.canvas.getHeight());
-        this.score = new Score(this.leftPlayerScore, this.rightPlayerScore);
-        this.startBtn = new StartBtn();
-    }
     draw() {
         this.canvas.clearCanvas();
         this.ball.draw(this.canvas.getContext());
-        this.leftPaddle.draw(this.canvas.getContext());
-        this.rightPaddle.draw(this.canvas.getContext());
+        this.leftPaddle_.draw(this.canvas.getContext());
+        this.rightPaddle_.draw(this.canvas.getContext());
         this.middleLine.draw(this.canvas.getContext());
         this.score.draw(this.canvas.getContext());
-        this.startBtn.start();
-        // Usage
-        const keyboard = new KeyboardInput();
-        // Example of how to use it in your game logic:
-        if (keyboard.isUpPressed()) {
-            // Perform an action when the "ArrowUp" key is pressed
+    }
+    update() {
+        // move paddles
+        if (this.upPressed && this.rightPaddle > 0) {
+            this.rightPaddle -= this.paddleSpeed;
         }
-        if (keyboard.isWPressed()) {
-            // Perform an action when the "w" key is pressed
+        else if (this.downPressed && this.rightPaddle + this.paddleHeight < this.canvas.getHeight()) {
+            this.rightPaddle += this.paddleSpeed;
+        }
+        // move right paddle automaticlly based on ball position
+        if (this.ballY > this.leftPaddle + this.paddleHeight / 2) {
+            this.leftPaddle += this.paddleSpeed;
+        }
+        else if (this.ballY < this.leftPaddle + this.paddleHeight / 2) {
+            this.leftPaddle -= this.paddleSpeed;
+        }
+        // move ball
+        this.ballX += this.ballSpeedX;
+        this.ballY += this.ballSpeedX;
+        // check if ball collides with top or bottom
+        if (this.ballY - this.ballRadius < 0 || this.ballY + this.ballRadius > this.canvas.getHeight()) {
+            this.ballSpeedY = -this.ballSpeedY;
+        }
+        // check if ball colides with left paddle
+        if (this.ballX - this.ballRadius < this.paddleWidth && this.ballY > this.leftPaddle && this.ballY < this.leftPaddle + this.paddleHeight) {
+            this.ballSpeedX = -this.ballSpeedX;
+        }
+        // check if ball collides with right paddle
+        if (this.ballX + this.ballRadius > this.canvas.getWidth() - this.paddleWidth && this.ballY > this.rightPaddle && this.ballY < this.rightPaddle + this.paddleHeight) {
+            this.ballSpeedX = -this.ballSpeedX;
+        }
+        // check if ball goes out of bounds on left or right side
+        if (this.ballX < 0) {
+            this.rightPlayerScore++;
+            this.reset();
+        }
+        else if (this.ballX > this.canvas.getWidth()) {
+            this.leftPlayerScore++;
+            this.reset();
+        }
+        // check if player has won
+        if (this.leftPlayerScore === 5) {
+            this.player = 'left player';
+            this.playerWin();
+        }
+        else if (this.rightPlayerScore == 5) {
+            this.player = 'right player';
+            this.playerWin();
+        }
+    }
+    playerWin() {
+        var message = "Congratulations! " + this.player + " win!";
+        (0, jquery_1.default)('#message').text(message); // Set the message text
+        (0, jquery_1.default)('#message-modal').modal('show'); // Display the message modal
+        this.reset();
+    }
+    reset() {
+        this.ballX = this.canvas.getWidth() / 2;
+        this.ballY = this.canvas.getHeight() / 2;
+        this.ballSpeedX = -this.ballSpeedX;
+        this.ballSpeedY = Math.random() * 10 - 5;
+    }
+    start() {
+        if (!this.isRunning) {
+            const gameLoop = () => {
+                this.update();
+                this.draw();
+                requestAnimationFrame(gameLoop);
+            };
+            this.isRunning = true;
+            gameLoop();
         }
     }
 }
-const game = new PongGame();
-function gameLoop() {
-    game.draw();
-    requestAnimationFrame(gameLoop);
-}
+const pongGame = new PongGame();
+pongGame.start();
