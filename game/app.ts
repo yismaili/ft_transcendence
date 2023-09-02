be9416357501fd7749faa9bb32c69ba233f984e2
yismaili <yismaili@student.1337.ma>
2023-09-02 14:32:37 +0100

    draw the object on canvas with classes

--- game/app.ts
+++ game/app.ts
@@ -1,55 +1,100 @@
-// init canvas
-const cnvs = document.getElementById('canvas') as HTMLCanvasElement;
-let cntx:any;
-if (cnvs.getContext){
-    cntx = cnvs.getContext('2d') as CanvasRenderingContext2D;
+
+class Canvas {
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+
+    constructor() {
+        this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        this.context = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+        this.canvas.width = 800; 
+        this.canvas.height = 600;
+    }
+
+    getContext(): CanvasRenderingContext2D {
+        return this.context;
+    }
+
+    clearCanvas() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    getWidth(): number {
+        return this.canvas.width;
+    }
+
+    getHeight(): number {
+        return this.canvas.height;
+    }
 }
 
-// define ball properties
-const ball_X: number = cnvs.width / 2;
-const ball_Y: number = cnvs.height / 2;
-const ballRadius: number = 10;
-const ballSpeed_X: number = 10;
-const ballSpeed_Y: number = 10;
+class Ball {
+    private x: number;
+    private y: number;
+    private radius: number;
+    private speedX: number;
+    private speedY: number;
 
-// define paddle properties
-const paddleHeight: number = 100;
-const paddleWidth: number = 10;
-const leftPaddle: number = cnvs.height / 2 - paddleHeight / 2;
-const rightPaddle: number = leftPaddle;
- 
-// define score properties
-let firstPlayerScore: number = 0;
-let scondPlayerScore: number = 0;
-const maxPlayerScore: number = 8;
+    constructor(x: number, y: number, radius: number, speedX: number, speedY: number) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
 
-function drawObject(){
-    // method clears an area of the canvas
-    cntx.clearRect(0, 0, cnvs.width, cnvs.height);
-
-    // color and style of ad
-    cntx.fillStyle = '#ffffff';
-    cntx.font = 'small-caps 18px Arial';
-
-    //  draw the line
-    cntx.beginPath();
-    cntx.moveTo(cnvs.width / 2, 0);
-    cntx.lineTo(cnvs.width / 2, cnvs.height);
-    cntx.strokeStyle = '#ffffff';
-    cntx.stroke();
-    cntx.closePath();
-
-    // draw the ball
-    cntx.beginPath();
-    cntx.arc(ball_X, ball_Y, ballRadius, 0, 6.28318530718);
-    cntx.fill();
-    cntx.closePath();
-
-    // draw right paddle
-    cntx.fillRect(cnvs.width - paddleWidth, rightPaddle, paddleWidth, paddleHeight);
-    // draw right paddle
-    cntx.fillRect(0, leftPaddle, paddleWidth, paddleHeight);
-
+    draw(context: CanvasRenderingContext2D) {
+        context.fillStyle = '#ffffff';
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.fill();
+        context.closePath();
+    }
 }
 
-drawObject();
+class Paddle {
+    private x: number;
+    private y: number;
+    private width: number;
+    private height: number;
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        context.fillStyle = '#ffffff';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class PongGame {
+    private canvas: Canvas;
+    private ball: Ball;
+    private leftPaddle: Paddle;
+    private rightPaddle: Paddle;
+
+    constructor() {
+        this.canvas = new Canvas();
+        this.ball = new Ball(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2, 10, 10, 10);
+        this.leftPaddle = new Paddle(0, this.canvas.getHeight() / 2 - 50, 10, 100);
+        this.rightPaddle = new Paddle(this.canvas.getWidth() - 10, this.canvas.getHeight() / 2 - 50, 10, 100);
+    }
+    
+    draw() {
+        this.canvas.clearCanvas();
+        this.ball.draw(this.canvas.getContext());
+        this.leftPaddle.draw(this.canvas.getContext());
+        this.rightPaddle.draw(this.canvas.getContext());
+    }
+}
+
+const game = new PongGame();
+function gameLoop() {
+    game.draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
