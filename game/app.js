284e7807425e8843e2e40e9f8a373114857df251
yismaili <yismaili@student.1337.ma>
2023-09-02 14:32:37 +0100

    draw the object on canvas with classes

--- game/app.js
+++ game/app.js
@@ -1,52 +1,72 @@
 "use strict";
-// init canvas
-const cnvs = document.getElementById('canvas');
-let cntx;
-if (cnvs.getContext) {
-    cntx = cnvs.getContext('2d');
+class Canvas {
+    constructor() {
+        this.canvas = document.getElementById('canvas');
+        this.context = this.canvas.getContext('2d');
+        // Set canvas size
+        this.canvas.width = 800;
+        this.canvas.height = 600;
+    }
+    getContext() {
+        return this.context;
+    }
+    clearCanvas() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+    getWidth() {
+        return this.canvas.width;
+    }
+    getHeight() {
+        return this.canvas.height;
+    }
 }
-// define ball properties
-const ball_X = cnvs.width / 2;
-const ball_Y = cnvs.height / 2;
-const ballRadius = 10;
-const ballSpeed_X = 10;
-const ballSpeed_Y = 10;
-// define paddle properties
-const paddleHeight = 100;
-const paddleWidth = 10;
-const leftPaddle = cnvs.height / 2 - paddleHeight / 2;
-const rightPaddle = leftPaddle;
-// define score properties
-let firstPlayerScore = 0;
-let scondPlayerScore = 0;
-const maxPlayerScore = 8;
-function drawObject() {
-    // method clears an area of the canvas
-    cntx.clearRect(0, 0, cnvs.width, cnvs.height);
-    // color ann style of font
-    cntx.fillStyle = '#ffffff';
-    cntx.font = 'small-caps 18px Arial';
-    // start a new path
-    cntx.beginPath();
-    // move the current drawing point to position(x, y)
-    cntx.moveTo(cnvs.width / 2, 0);
-    // draw a line to position(xn, yn)
-    cntx.lineTo(cnvs.width / 2, cnvs.height);
-    // set line color to white
-    cntx.strokeStyle = '#ffffff';
-    // stroke the path to make it visible on the canvas
-    cntx.stroke();
-    // close the path by connecting back to the starting point
-    cntx.closePath();
-    // draw the ball
-    cntx.beginPath();
-    cntx.arc(ball_X, ball_Y, ballRadius, 0, 6.28318530718);
-    // fill the closed shape
-    cntx.fill();
-    cntx.closePath();
-    // draw right paddle
-    cntx.fillRect(cnvs.width - paddleWidth, rightPaddle, paddleWidth, paddleHeight);
-    // draw right paddle
-    cntx.fillRect(0, leftPaddle, paddleWidth, paddleHeight);
+class Ball {
+    constructor(x, y, radius, speedX, speedY) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+    draw(context) {
+        context.fillStyle = '#ffffff';
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.fill();
+        context.closePath();
+    }
 }
-drawObject();
+class Paddle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw(context) {
+        context.fillStyle = '#ffffff';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+class PongGame {
+    constructor() {
+        this.canvas = new Canvas();
+        this.ball = new Ball(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2, 10, 10, 10);
+        this.leftPaddle = new Paddle(0, this.canvas.getHeight() / 2 - 50, 10, 100);
+        this.rightPaddle = new Paddle(this.canvas.getWidth() - 10, this.canvas.getHeight() / 2 - 50, 10, 100);
+    }
+    draw() {
+        this.canvas.clearCanvas();
+        this.ball.draw(this.canvas.getContext());
+        this.leftPaddle.draw(this.canvas.getContext());
+        this.rightPaddle.draw(this.canvas.getContext());
+        // Add more drawing logic for scores, lines, etc.
+    }
+}
+const game = new PongGame();
+function gameLoop() {
+    game.draw();
+    // Add game logic here, updating ball and paddle positions
+    requestAnimationFrame(gameLoop);
+}
+gameLoop();
