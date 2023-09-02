a5fb3dd422bfa9479f1fe8844ccd937802f1b3c4
yismaili <yismaili@student.1337.ma>
2023-09-02 11:57:05 +0100

    draw the object on canvas

--- game/app.ts
+++ game/app.ts
@@ -23,8 +23,33 @@ let firstPlayerScore: number = 0;
 let scondPlayerScore: number = 0;
 const maxPlayerScore: number = 8;
 
-void function drowObject(){
+function drawObject(){
     // method clears an area of the canvas
     cntx.clearRect(0, 0, cnvs.width, cnvs.height);
-    
+
+    // color and style of ad
+    cntx.fillStyle = '#ffffff';
+    cntx.font = 'small-caps 18px Arial';
+
+    //  draw the line
+    cntx.beginPath();
+    cntx.moveTo(cnvs.width / 2, 0);
+    cntx.lineTo(cnvs.width / 2, cnvs.height);
+    cntx.strokeStyle = '#ffffff';
+    cntx.stroke();
+    cntx.closePath();
+
+    // draw the ball
+    cntx.beginPath();
+    cntx.arc(ball_X, ball_Y, ballRadius, 0, 6.28318530718);
+    cntx.fill();
+    cntx.closePath();
+
+    // draw right paddle
+    cntx.fillRect(cnvs.width - paddleWidth, rightPaddle, paddleWidth, paddleHeight);
+    // draw right paddle
+    cntx.fillRect(0, leftPaddle, paddleWidth, paddleHeight);
+
 }
+
+drawObject();
