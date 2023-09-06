2147e574785b5721c9a7bf8eebd6ebd6ea7f9ae3
yismaili <yismaili@student.1337.ma>
2023-09-06 21:06:44 +0100

    start to implement the game with socket io

--- back_end/transcendence/utils/types.ts
+++ back_end/transcendence/utils/types.ts
@@ -50,3 +50,26 @@ export interface IAuthenticate {
   token: string;
   user: User;
 }
+
+export  interface updateGame {
+    leftPaddle: number;
+    rightPaddle: number;
+    paddleWidth: number;
+    ballSpeedX: number;
+    ballSpeedY: number;
+    paddleHeight: number;
+    ballRadius: number;
+    paddleSpeed: number;
+    upPressed: boolean;
+    downPressed: boolean;
+    wPressed: boolean;
+    sPressed: boolean;
+    score: number;
+    ballX: number;
+    ballY: number;
+    rightPlayerScore:number;
+    leftPlayerScore: number;
+    player: string;
+    canvasHeight:number;
+    canvasWidth:number;
+}
