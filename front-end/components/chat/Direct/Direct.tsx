26da895340a2d0bfd85d21d6c2ba2d7faca56e72
alouane04 <ariahi@student.1337.ma>
2023-09-24 19:08:37 +0100

    i fix the rendering for multiple users and i still have to wait for yismaili to fix history order

--- front-end/components/chat/Direct/Direct.tsx
+++ front-end/components/chat/Direct/Direct.tsx
@@ -1,17 +1,24 @@
 import Style from "./Direct.module.css";
 
 type props = {
-  data: UserFriend,
-  choseChat: Function
-}
+  data: UserFriend;
+  choseChat: Function;
+};
 
 export default function Direct({ data, choseChat }: props) {
-  const choseConversation = () => choseChat(data);
+  // console.log('data from', data);
+
+  const choseConversation = () => {
+    choseChat(data);
+  };
+
   return (
     <div className={Style.container} onClick={choseConversation}>
       <div className={Style.imgContainer}>
-      <div className={Style.avatar} style={{ backgroundImage: `url("${data.user.picture}")` }}>
-      </div>
+        <div
+          className={Style.avatar}
+          style={{ backgroundImage: `url("${data.user.picture}")` }}
+        ></div>
         <div className={Style.onlineStatus}></div>
       </div>
       <p className={Style.name}>{data.user.username}</p>
