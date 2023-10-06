190bb94ada1ea1cacb01f5cb6a6673c8123fef40
alouane04 <ariahi@student.1337.ma>
2023-10-06 19:06:31 +0100

    the message is sent to database but socket is not return response

--- front-end/components/chat/GroupMsg/GroupMsg.tsx
+++ front-end/components/chat/GroupMsg/GroupMsg.tsx
@@ -1,18 +1,24 @@
 import Style from "./GroupMsg.module.css";
 import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import io from "socket.io-client";
 import Cookies from "cookies-ts";
+import LeftChatGroup from "./LeftChatGroup/LeftChatGroup";
+import RightChatGroup from "./RightChatGroup/RightChatGroup";
+import InputChatGroup from "./InputChatGroup/InputChatGroup";
 
 type props = {
   groupInput: GroupInput | undefined;
   // setData: Function;
   room: AllRooms;
+  setMessage: Function;
 };
 
 export default function GroupMsg({
   groupInput,
   // setData,
   room,
+  setMessage,
 }: props) {
   // const cookies = new Cookies();
   // const Data = JSON.parse(JSON.stringify(cookies.get("userData")));
@@ -29,5 +35,51 @@ export default function GroupMsg({
   //   })
   // );
 
-  return <div>{room.name}</div>;
+  return (
+    <div className={Style.container}>
+      {/* <motion.ul initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }}>
+        {allMessages &&
+          allMessages.map((message, index) => {
+            const isLastMessage = index === allMessages.length - 1;
+            return (
+              <li key={message.id}>
+                {message.user.username == myData.data.username ? (
+                  <RightChatGroup oldMessage={message} newMessage={undefined} />
+                ) : (
+                  <LeftChatGroup
+                    oldMessage={message}
+                    newMessage={undefined}
+                    friendData={friendData}
+                  />
+                )}
+                {isLastMessage && <div ref={ref} />}
+              </li>
+            );
+          })}
+        {newMessage &&
+          newMessage.map((message, index) => {
+            const isLastMessage = index === newMessage.length - 1;
+            console.log("test", newMessage);
+
+            return (
+              <li key={message.id}>
+                {message.user.username == myData.data.username ? (
+                  <RightChatGroup oldMessage={undefined} newMessage={message} />
+                ) : (
+                  <LeftChatGroup
+                    oldMessage={undefined}
+                    newMessage={message}
+                    friendData={friendData}
+                  />
+                )}
+                {isLastMessage && <div ref={ref} />}
+              </li>
+            );
+          })}
+      </motion.ul>
+      <InputChat socket={socket} setMessage={setMessage} /> */}
+      <InputChatGroup setMessage={setMessage} />
+      test
+    </div>
+  );
 }
