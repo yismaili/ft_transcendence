771c8d9cd68743a624b3f49d8eb39270411abd95
alouane04 <ariahi@student.1337.ma>
2023-10-20 19:06:43 +0100

    import nav after update for responsive

--- front-end/components/nav/Nav.tsx
+++ front-end/components/nav/Nav.tsx
@@ -3,54 +3,123 @@ import Image from "next/image";
 import { Link as Scroll_link } from "react-scroll";
 import Link from "next/link";
 import Style from "./Nav.module.css";
+import { useState } from "react";
 
-export default function Nav() {
+interface navState {
+  num: number;
+  seter: Function;
+}
+export default function Nav(prop: navState) {
+  const [phoneNav, setPhoneNav] = useState(false);
+
   return (
-    <div className={Style.container}>
-      <Scroll_link to="section1" style={{ cursor: "pointer" }} smooth={true}>
-        <div className={Style.imageRes}>
-          <Image
-            quality={100}
-            className={Style.imageRes}
-            width={75}
-            height={75}
-            src="/logo.png"
-            alt="logo for pong game"
-            priority={true}
-          />
-          <h1 className={Style.title}>PONG</h1>
-        </div>
-      </Scroll_link>
-      <div className={Style.links}>
+    <>
+      <div className={Style.container}>
         <Scroll_link
-          id="link1"
           to="section1"
           style={{ cursor: "pointer" }}
           smooth={true}
+          onClick={() => prop.seter(0)}
         >
-          home
+          <div className={Style.imageRes}>
+            <Image
+              quality={100}
+              className={Style.imageRes}
+              width={75}
+              height={75}
+              src="/logo.png"
+              alt="logo for pong game"
+              priority={true}
+            />
+            <h1 className={Style.title}>PONG</h1>
+          </div>
         </Scroll_link>
-        <Scroll_link
-          id="link2"
-          to="section2"
-          style={{ cursor: "pointer" }}
-          smooth={true}
-        >
-          about us
-        </Scroll_link>
-        <Scroll_link
-          id="link4"
-          to="section4"
-          style={{ cursor: "pointer" }}
-          smooth={true}
-        >
-          faq
-        </Scroll_link>
-        <Link href="/login" style={{ cursor: "pointer" }}>
-          Sign up
-        </Link>
+        <div className={Style.links}>
+          <Scroll_link
+            id="link1"
+            to="section1"
+            style={{ cursor: "pointer" }}
+            smooth={true}
+            onClick={() => prop.seter(0)}
+          >
+            home
+            {prop.num == 0 && <div className={Style.bar}></div>}
+          </Scroll_link>
+          <Scroll_link
+            id="link2"
+            to="section2"
+            style={{ cursor: "pointer" }}
+            smooth={true}
+            onClick={() => prop.seter(1)}
+          >
+            about us
+            {prop.num == 1 && <div className={Style.bar}></div>}
+          </Scroll_link>
+          <Scroll_link
+            id="link4"
+            to="section4"
+            style={{ cursor: "pointer" }}
+            smooth={true}
+            className={Style.ok}
+            onClick={() => prop.seter(2)}
+          >
+            faq
+            {prop.num == 2 && <div className={Style.bar}></div>}
+          </Scroll_link>
+          <Link href="/login" style={{ cursor: "pointer" }}>
+            Sign up
+          </Link>
+        </div>
+        <div className={Style.phoneNav}>
+          {!phoneNav && (
+            <span
+              className={Style.openNavIcon}
+              onClick={() => setPhoneNav(true)}
+            ></span>
+          )}
+          {phoneNav && (
+            <span
+              className={Style.closeNav}
+              onClick={() => setPhoneNav(false)}
+            ></span>
+          )}
+        </div>
+        {phoneNav && (
+          <div className={Style.phoneLinks}>
+            <Scroll_link
+              id="link1"
+              to="section1"
+              style={{ cursor: "pointer" }}
+              smooth={true}
+              onClick={() => prop.seter(0)}
+            >
+              home
+            </Scroll_link>
+            <Scroll_link
+              id="link2"
+              to="section2"
+              style={{ cursor: "pointer" }}
+              smooth={true}
+              onClick={() => prop.seter(1)}
+            >
+              about us
+            </Scroll_link>
+            <Scroll_link
+              id="link4"
+              to="section4"
+              style={{ cursor: "pointer" }}
+              smooth={true}
+              className={Style.ok}
+              onClick={() => prop.seter(2)}
+            >
+              faq
+            </Scroll_link>
+            <Link href="/login" style={{ cursor: "pointer" }}>
+              Sign up
+            </Link>
+          </div>
+        )}
       </div>
-      <div className={Style.bar}></div>
-    </div>
+    </>
   );
 }
