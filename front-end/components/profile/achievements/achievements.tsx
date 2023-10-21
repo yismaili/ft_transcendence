fd36257c048e3f895128395c2a520e4ccc791421
alouane04 <ariahi@student.1337.ma>
2023-10-21 14:54:32 +0100

    pull from merged master

---
+++ front-end/components/profile/achievements/achievements.tsx
@@ -0,0 +1,16 @@
+"use client"
+import "./achievements.css";
+
+type props = {
+  isDisplay: boolean;
+};
+
+function Achievements({ isDisplay }: props) {
+  return (
+    <div className={`achievements ${isDisplay ? "show__Achievement" : "noshow__Achievement"}`}>
+      <h3 className="achievements__title">achievements</h3>
+    </div>
+  );
+}
+
+export default Achievements;
