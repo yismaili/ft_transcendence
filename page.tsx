e2dd6a2908807ddfd3d08ccd5a6d17c450876f2d
El amine El mountassir <eel-moun@e2r12p3.1337.ma>
2023-10-21 23:36:08 +0100

    filling holes and solving problems

---
+++ page.tsx
@@ -0,0 +1,6 @@
+import "../../../styles/global.css";
+import UserDashboard from "@/components/UserDashboard/UserDashboard";
+
+export default function Users({ params }: { params: { user: string } }) {
+  return <UserDashboard params={params} />;
+}
