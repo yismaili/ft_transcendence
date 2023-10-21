877b758211f504c6a8ddf376348de3a7f890b585
El amine El mountassir <eel-moun@e2r12p3.1337.ma>
2023-10-21 19:00:50 +0100

    solved a lot of problems about to pull from master

--- front-end/components/profile/Parameters/2FA_popup/enable/Enable__2FA.tsx
+++ front-end/components/profile/Parameters/2FA_popup/enable/Enable__2FA.tsx
@@ -61,6 +61,7 @@ export default function Enable2FAPopUP(prop:twofa){
                 <button className="auth__2fa__form__btn">confirm</button>
             </form>
             {warning === 1 && <p style={{color: "red"}}>Wrong 2FA Code</p>}
+            <span className="close__btn" onClick={() => prop.state(0)}></span>
         </div>
         </div>
     );
