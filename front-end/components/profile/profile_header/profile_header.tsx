439418c7c6ca782e1de1ac32da8f5d8868232faa
El amine El mountassir <eel-moun@e2r12p3.1337.ma>
2023-10-21 23:36:08 +0100

    filling holes and solving problems

--- front-end/components/profile/profile_header/profile_header.tsx
+++ front-end/components/profile/profile_header/profile_header.tsx
@@ -4,12 +4,14 @@ import "@/global_css/utilityClasses.css";
 import "./App-header.css";
 import Link from "next/link";
 
-
-export default function ProfileHeader(){
+interface userN{
+  path:string
+}
+export default function ProfileHeader(prop : userN){
   return (<>
     <header className="app__header">
             <Link className="app__header__chat opac" href="/chat">chatroom</Link>
-            <Link className="app__header__profile" href="/UserProfile">profile</Link>
+            <Link className="app__header__profile" href={`/UserProfile/${prop.path}`}>profile</Link>
     </header>
   </>);
 }
