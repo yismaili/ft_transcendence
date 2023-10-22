915249f2c6127727b6d49fa613c2ff42cd896656
alouane04 <ariahi@student.1337.ma>
2023-10-22 22:14:20 +0100

    on fixing the chat

--- front-end/components/chat/Msg/Msg.tsx
+++ front-end/components/chat/Msg/Msg.tsx
@@ -19,6 +19,8 @@ export default function Msg({ friendData, myData }: props) {
 
   useEffect(() => {
     socket.on("message", (message: allMessages[]) => {
+      console.log('sse');
+      
       setNewMessage((prevMessages) => [...prevMessages, message[0]]);
     });
 
