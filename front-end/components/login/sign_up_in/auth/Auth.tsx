2e5ecddbe55f1a1d4f89afeeca96a4ef545b6309
El amine El mountassir <eel-moun@e2r9p4.1337.ma>
2023-10-24 14:07:53 +0100

    about to pull from master

--- front-end/components/login/sign_up_in/auth/Auth.tsx
+++ front-end/components/login/sign_up_in/auth/Auth.tsx
@@ -20,17 +20,19 @@ export default function Auth({ Sign_in_up }: Props) {
       let interval = setInterval(() => {
         const cookies = new Cookies();
         const mycookie = cookies.get("userData");
+        console.log("test753:", JSON.parse(JSON.stringify(mycookie)).j);
 
         if (mycookie) {
           clearInterval(interval);
           let cookie = mycookie.slice(2);
+          console.log("test741:", cookie);
           let cookieval = JSON.parse(mycookie.slice(2));
           cookies.set("userData", cookie);
           auth_window?.close();
           if (cookieval.response.user.isTwoFactorAuthEnabled)
           router.push("http://localhost:3000/login/2FA");
         else router.push(`http://localhost:3000/UserProfile/${cookieval.response.user.username}`);
-        console.clear();
+        // console.clear();
         }
       }, 1000);
     }
