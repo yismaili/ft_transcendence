a3b3e4077b27c25dc977d6aa54cb01763e0c0547
alouane04 <ariahi@student.1337.ma>
2023-10-25 16:51:56 +0100

    one socket on all pages and exclude landing/login/2fa pages

--- front-end/app/users/[user]/page.tsx
+++ front-end/app/users/[user]/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import UserProfile from "@/components/profile/UserProfile/UserProfile";
-import SocketContextProvider from "@/contexts/socket-context";
 
 export default function Profile({ params }: { params: { user: string } }) {
-  return (
-    <SocketContextProvider>
-      <UserProfile params={params} />
-    </SocketContextProvider>
-  );
+  return <UserProfile params={params} />;
 }
