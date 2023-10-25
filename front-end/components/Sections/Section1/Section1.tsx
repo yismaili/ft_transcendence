de9a0ff1545afd4dd04812dc19a232779fe7d3e5
alouane04 <ariahi@student.1337.ma>
2023-10-25 18:18:27 +0100

    navigation optimisation

--- front-end/components/Sections/Section1/Section1.tsx
+++ front-end/components/Sections/Section1/Section1.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Style from "./Section1.module.css";
 import { useEffect } from "react";
-import "@/global_css/utilityClasses.css"
+import "@/global_css/utilityClasses.css";
 import Link from "next/link";
+import Cookies from "cookies-ts";
 
 export default function Section1() {
+  const cookies = new Cookies();
+  const Data = JSON.parse(JSON.stringify(cookies.get("userData")));
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > -1) {
@@ -28,12 +32,12 @@ export default function Section1() {
         <h1 className={Style.title}>
           FUTURE OF <span>PONG</span> START NOW
         </h1>
-        <Link href={"/login"}>
-          <button >PLAY NOW</button>
-          </Link>
+        <Link href={Data ? `/users/${Data.response.user.username}` : `/login`}>
+          <button>PLAY NOW</button>
+        </Link>
       </div>
       <div className={Style.MainImg}>
-          <span className={Style.ball}></span>
+        <span className={Style.ball}></span>
         <span className={Style.Moon}></span>
       </div>
     </section>
