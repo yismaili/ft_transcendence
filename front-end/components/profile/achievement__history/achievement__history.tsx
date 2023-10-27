992e7fb6d5b05a933551b04a0061957eefce0c2a
El amine El mountassir <eel-moun@e2r11p2.1337.ma>
2023-10-27 20:54:33 +0100

    working on history

--- front-end/components/profile/achievement__history/achievement__history.tsx
+++ front-end/components/profile/achievement__history/achievement__history.tsx
@@ -2,10 +2,20 @@
 import Achievements from "@/components/profile/achievements/achievements";
 import History from "@/components/profile/history/history";
 import "./achievement__history.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function History__Achievements() {
   const [achivment, setAchivement] = useState(true);
+  
+  useEffect(() => {
+    const fetching = async () => {
+      const res = await fetch("http://localhost:3000/api/home/history");
+      const history = await res.json();
+      console.log("history:",history.data[0]);
+    }
+    fetching();
+  });
+
   return (
     <div className="achievements__history">
       <Achievements isDisplay={achivment} />
