8ba1a239c7a2afd41bf5baca7d93d1053d724be5
El amine El mountassir <eel-moun@e2r11p2.1337.ma>
2023-10-28 01:59:30 +0100

    about to push to master

--- front-end/components/profile/history/history.tsx
+++ front-end/components/profile/history/history.tsx
@@ -8,24 +8,39 @@ import { useSocketContext } from "@/contexts/socket-context";
 
 type props = {
   isDisplay: boolean;
+  ownerName: string;
 };
 
-function History({ isDisplay }: props) {
+function History({ isDisplay, ownerName }: props) {
   const { Data } = useSocketContext();
   const [hist, sethistory] = useState<history[]>();
 
   useEffect(() => {
     const fetching = async () => {
-      const res = await fetch("http://localhost:3000/api/home/history");
-      const val = await res.json();
-      if (val) {
-        sethistory(val.data);
+      if(ownerName == Data.response.user.username)
+      {
+        console.log("ana wst l owner");
+        const res = await fetch("http://localhost:3000/api/home/history");
+        const val = await res.json();
+        if (val) {
+          sethistory(val.data);
+        }
+      }else{
+        console.log("ana wst l friend");
+        const res = await fetch("http://localhost:3000/api/friend/history", {
+          method: "POST",
+          body: ownerName,
+        });
+        const val = await res.json();
+        if (val) {
+          sethistory(val.data);
+        }
       }
-      console.log("his", hist);
+       
     };
     fetching();
   }, []);
-  if (hist) {
+
     return (
       <div
         className={`history ${
@@ -33,8 +48,8 @@ function History({ isDisplay }: props) {
         }`}
       >
         <h3 className="history__title">history</h3>
-        {hist.map((pastGame) => {
-            if(pastGame.user.username == Data.response.user.username)
+        {hist && hist.map((pastGame) => {
+            if(pastGame.user.username == ownerName)
               return <SingleHistory Player1={pastGame.user} Player2={pastGame.userCompetitor} Player1Score={pastGame.resulteOfUser} Player2Score={pastGame.resulteOfCompetitor}/>;
             else
               return <SingleHistory Player2={pastGame.user} Player1={pastGame.userCompetitor} Player2Score={pastGame.resulteOfUser} Player1Score={pastGame.resulteOfCompetitor}/>;
@@ -43,6 +58,6 @@ function History({ isDisplay }: props) {
       </div>
     );
   }
-}
+
 
 export default History;
