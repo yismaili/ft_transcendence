b1bc65025e5ea3fa292242f95d88fb5b83bff7c4
El amine El mountassir <eel-moun@e2r11p2.1337.ma>
2023-10-28 17:47:50 +0100

    finished achievements and history

--- front-end/components/profile/achievements/achievements.tsx
+++ front-end/components/profile/achievements/achievements.tsx
@@ -9,6 +9,20 @@ function Achievements({ isDisplay }: props) {
   return (
     <div className={`achievements ${isDisplay ? "show__Achievement" : "noshow__Achievement"}`}>
       <h3 className="achievements__title">achievements</h3>
+      <div className="achievements__list">
+        <div className="achievements__list__ach">
+          <span className="achievements__list__trophy silver"></span>
+          <h3 className="achievements__list__ach__title"> first game</h3>
+        </div>
+        <div className="achievements__list__ach">
+          <span className="achievements__list__trophy goldStar"></span>
+          <h3 className="achievements__list__ach__title"> first win</h3>
+        </div>
+        <div className="achievements__list__ach">
+          <span className="achievements__list__trophy gold"></span>
+          <h3 className="achievements__list__ach__title">first loss</h3>
+        </div>
+      </div>
     </div>
   );
 }
