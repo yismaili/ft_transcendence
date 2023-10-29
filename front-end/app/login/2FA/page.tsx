6b3038d71d004054c5b05f5140e170ab4ae3a05b
alouane04 <ariahi@student.1337.ma>
2023-10-29 17:45:00 +0100

    start test deployment

--- front-end/app/login/2FA/page.tsx
+++ front-end/app/login/2FA/page.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+"use client";
+import React from "react";
 import "@/global_css/resets.css";
 import "@/global_css/utilityClasses.css";
-import Auth_2fa from '@/components/auth/auth_2FA/auth_2fa';
+import { useEffect, useState } from "react";
+import Auth_2fa from "@/components/auth/auth_2FA/auth_2fa";
 
 function App() {
-    return (<>
-    <Auth_2fa />
-    </>);
-  }
-  
-  export default App;
+  const [test, setTest] = useState(false);
+
+  useEffect(() => {
+    setTest(true);
+  }, []);
+  return test && <Auth_2fa />;
+}
+
+export default App;
