35739746d829f98886de20fa388e90c816a07cc6
El amine El mountassir <eel-moun@e2r11p2.1337.ma>
2023-10-29 23:58:31 +0100

    run frontend on container

--- front-end/app/api/updateProfile/username/route.ts
+++ front-end/app/api/updateProfile/username/route.ts
@@ -2,25 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function POST(req: NextRequest) {
-    const cookieStore = cookies();
-    const token = cookieStore.get("userData");
-    const res = JSON.stringify({uniquename : await req.json()});
-    if (token) {
-        const cookieObject = JSON.parse(token.value);
-        const data = await fetch(
-          `http://${process.env.NEXT_PUBLIC_HOST_PORT}/users/profile/${cookieObject.response.user.username}/addUniquename`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${cookieObject.response.token}`,
-            },
-            body: res,
-          }
-        );
-        const isSent = await data.json();
-        if(isSent.uniquename)
-          return NextResponse.json("done");
+  const cookieStore = cookies();
+  const token = cookieStore.get("userData");
+  const res = JSON.stringify({ uniquename: await req.json() });
+  if (token) {
+    const cookieObject = JSON.parse(token.value);
+    const data = await fetch(
+      `http://backend:3001/users/profile/${cookieObject.response.user.username}/addUniquename`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookieObject.response.token}`,
+        },
+        body: res,
       }
-      return NextResponse.json("error");
+    );
+    const isSent = await data.json();
+    if (isSent.uniquename) return NextResponse.json("done");
+  }
+  return NextResponse.json("error");
 }
